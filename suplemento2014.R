14cb77f06a0bdd3cc215071144cb6ce2232cd0d3
Lhayana Vieira <lhayana11@gmail.com>
2023-05-16 22:21:51 -0300

    Update suplemento2014.R

--- suplemento2014.R
+++ suplemento2014.R
@@ -1,7 +1,10 @@
 # SUPLEMENTO: https://www.ibge.gov.br/estatisticas/sociais/trabalho/19898-suplementos-pnad3.html?edicao=17983&t=downloads
 
 library('microdadosBrasil')
+library('dplyr')
 
-suplemento = read_PNAD('pessoas', 2014, file = path.expand("C:/Users/Lhayana/Documents/pnad_2014_data/suplemento/PES2014.txt"))
+pessoas = read_PNAD('pessoas', 2014, file = path.expand("C:/Users/Lhayana/Documents/pnad_2014_data/suplemento/PES2014.txt"))
+domicilios = read_PNAD('domicilios', 2014, file = path.expand("C:/Users/Lhayana/Documents/pnad_2014_data/suplemento/DOM2014.txt"))
 
-suplemento$UF = substr(suplemento$V0102,1,2)
+pessoas = select(suplemento,UF,V0102,V0103,V0404,V0502,V4750,V4742,V4718,V4719,V4720,V4721,V4722,V0305,V32000,V32001,V32002)
+domicilios = select(domicilios, V0102,V0103,V4602,UPA)
