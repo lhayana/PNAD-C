552378c9fdf5431444c73e44ba31dd345991f717
Lhayana Vieira <lhayana11@gmail.com>
2023-05-24 23:29:02 -0300

    Update suplemento2014.R

--- suplemento2014.R
+++ suplemento2014.R
@@ -11,7 +11,7 @@ library(ggplot2)
 pessoas = read_PNAD('pessoas', 2014, file = path.expand("C:/Users/Lhayana/Documents/pnad_2014_data/suplemento/PES2014.txt"))
 domicilios = read_PNAD('domicilios', 2014, file = path.expand("C:/Users/Lhayana/Documents/pnad_2014_data/suplemento/DOM2014.txt"))
 # V4742 = rendimento dom. per capita
-pessoas = select(suplemento,UF,V0102,V0103,V4729,V0404,V0502,V4750,V4742,V4718,V4719,V4720,V4721,V4722,V0305,V32000,V32001,V32002)
+pessoas = select(pessoas,UF,V0102,V0103,V32039,V0404,V4742,V0305,V32001,V32004,V32010,V32012, V32024, V32026,V4743)
 domicilios = select(domicilios, V0102,V0103,V4602,UPA)
 
 base = merge(x=pessoas,y=domicilios,by=c("V0102","V0103"))
@@ -19,9 +19,14 @@ base = merge(x=pessoas,y=domicilios,by=c("V0102","V0103"))
 base$V4742 = sub("^0+", "", base$V4742) #tira zeros da esquerda  
 base$V4742 = as.integer(base$V4742)
 base$V4602 = as.numeric(base$V4602)
+base$V32039 = as.numeric(base$V32039)
+
+base = filter(base, V0305==2) #Selecionados para o Suplemento de Mobilidade Sócio-Ocupacional
+
+base = rename(base, Estrato = V4602, peso = V32039, cor = V0404, renda_dom=V4742, renda_faixa=V4743, morava_uf_aos15=V32001, zona=V32004, morava_pai=V32010, escolaridade_pai=V32012, morava_mae=V32024, escolaridade_mae=V32026)
 
 base %>% #contagem populacional, nao amostral, pois está com peso
   filter(UF == 24) %>%
-  ggplot(aes(V4742, weight = V4602)) +
+  ggplot(aes(renda_dom, weight = peso)) +
   geom_freqpoly(binwidth = 998)
   
